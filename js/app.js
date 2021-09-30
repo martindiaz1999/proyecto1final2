8a78fbc44b5129451e9eed5a7c07880fcfa87c1b
martindiaz1999 <martindiaz235@gmail.com>
2021-09-30 11:32:25 -0300

    30-9

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/*
+console.log("ksjgslkjgsl")
+document.write("<h1>kdgjsioj</h1>")
+
+let martin = "buen dia"
+console.log(martin)
+martin = "buenas noches"
+console.log(martin)
+
+let martin5 = "dia jueves"
+console.log(martin5)
+
+martin5 = "martin5"
+console.log(martin5)
+
+let numero5 = 78
+console.log(numero5)
+
+numero5 = 99
+console.log(numero5)
+
+const PI = 3,14
+console.log(PI)
+
+const MES ="julio"
+console.log(MES)
+
+VER LO DE IF
+FINCIONES: CONTIENEN UN CODIGO QUE SE EJECUTA SOLO CUANDO EN ALGUN MOEMENTO LLAMAMOS ESA FUNCION, ME SIRVE PARA
+PONER OPERACIONES DENTRO DE ESA FUNCION Y NO TENER QUE VOLVER A ESCRIBIRLAS DESPUES, SOLO ESCRIBO LA FUNCION
+EL PROGRAMA SIEMPRE LEE DE ARRIBA PARA ABAJO, ENTONCES LA FUNCUIN SIEMPRE DEBE ESTAR ARRUBA
+-SUBIDO EN CLASE 10
+
+   */
+
+ let num1 = 99
+ let num2 = 55
+ let num3 = 10
+ let num4 = 22
+
+let suma = num4 + num2
+console.log (suma)
