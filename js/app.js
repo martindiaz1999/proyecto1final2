8e7c9ead5cd728398b55b1c2c65aa125da54294b
martindiaz1999 <martindiaz235@gmail.com>
2021-10-12 11:44:10 -0300

    30-9

--- js/app.js
+++ js/app.js
@@ -1,42 +1,22 @@
-/*
-console.log("ksjgslkjgsl")
-document.write("<h1>kdgjsioj</h1>")
 
-let martin = "buen dia"
-console.log(martin)
-martin = "buenas noches"
-console.log(martin)
+ $(document).ready(function(){
 
-let martin5 = "dia jueves"
-console.log(martin5)
+    $("#calcular").click(function(){
 
-martin5 = "martin5"
-console.log(martin5)
+      let valoruno = $("#valor1").val();
+      valoruno = parseFloat(valoruno);
+      
 
-let numero5 = 78
-console.log(numero5)
+      let valordos = $("#valor2").val();
+      valordos = parseFloat(valordos);
 
-numero5 = 99
-console.log(numero5)
+      let valortres = $("#valor3").val();
+      console.log(valortres);
+      
 
-const PI = 3,14
-console.log(PI)
+    })
+    
+ 
 
-const MES ="julio"
-console.log(MES)
 
-VER LO DE IF
-FINCIONES: CONTIENEN UN CODIGO QUE SE EJECUTA SOLO CUANDO EN ALGUN MOEMENTO LLAMAMOS ESA FUNCION, ME SIRVE PARA
-PONER OPERACIONES DENTRO DE ESA FUNCION Y NO TENER QUE VOLVER A ESCRIBIRLAS DESPUES, SOLO ESCRIBO LA FUNCION
-EL PROGRAMA SIEMPRE LEE DE ARRIBA PARA ABAJO, ENTONCES LA FUNCUIN SIEMPRE DEBE ESTAR ARRUBA
--SUBIDO EN CLASE 10
-
-   */
-
- let num1 = 99
- let num2 = 55
- let num3 = 10
- let num4 = 22
-
-let suma = num4 + num2
-console.log (suma)
+ })
